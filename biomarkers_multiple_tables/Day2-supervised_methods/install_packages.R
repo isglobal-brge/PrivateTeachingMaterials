d792b45779c1e6abcf37d10c52e0b03fe2fde289
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2019-05-12 21:32:03 +0200

    uploaded meta_analysis

--- biomarkers_multiple_tables/Day2-supervised_methods/install_packages.R
+++ biomarkers_multiple_tables/Day2-supervised_methods/install_packages.R
@@ -1,9 +1,20 @@
-install.packages(c("pdfCluster",
-"MASS",
-"rpart",
-"e1071",
-"nnet",
-"randomForest",
-"adabag",
-"flexclust",
-"pROC"))
+# Necessary to install packages from Bioconductor
+source("https://bioconductor.org/biocLite.R")
+
+# Day 1
+install.packages(c("devtools", "roxygen2"))
+
+# Day 2
+install.packages()
+
+# Day 3
+install.packages(c("nFactors", "cluster"))
+biocLite(c("made4", "impute"))
+
+# Day 3
+install.packages(c("pdfCluster", "MASS", "rpart", "e1071",
+"nnet", "randomForest", "adabag", "flexclust","pROC"))
+
+# Day 4
+install.packages(c("abn", "graph", "ggm"))
+biocLite("Rgraphviz")
